4f6d1bf22306cf44647021c016dbd19477cdaa05
islamElbadawy <islam.201211@gmail.com>
2024-01-08 16:35:23 +0200

    Initialize project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { QualificationsComponent } from './components/qualifications/qualifications.component';
+import { ServicesComponent } from './components/services/services.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    SkillsComponent,
+    FooterComponent,
+    ContactComponent,
+    PortfolioComponent,
+    ProjectsComponent,
+    QualificationsComponent,
+    ServicesComponent,
+    TestimonialsComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'my-portfolio';
+  title = 'Islam Elbadawy';
 }
---
+++ src/assets/js/main.js
@@ -0,0 +1,190 @@
+/*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = document.getElementById('nav__menu'),
+      navToggle = document.getElementById('nav__toggle'),
+      navClose = document.getElementById('nav__close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav-link');
+
+function linkAction(){
+    const navMenu = document.getElementById('nav__menu');
+
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach( n => n.addEventListener('click', linkAction))
+
+/*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills-content'),
+      skillsHeader = document.querySelectorAll('.skills-header')
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+    for(i=0; i<skillsContent.length; i++){
+        skillsContent[i].className = 'skills-content skills-close'
+    }
+
+    if(itemClass == 'skills-content skills-close'){
+        this.parentNode.className = 'skills-content skills-open'
+    }
+}
+
+skillsHeader.forEach((el) => el.addEventListener('click', toggleSkills))
+
+
+/*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification-active')
+        })
+        target.classList.add('qualification-active');
+
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification-active')
+        })
+        tab.classList.add('qualification-active')
+    })
+})
+
+/*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll('.services-modal'),
+      modalBtns = document.querySelectorAll('.services-button'),
+      modalClose = document.querySelectorAll('.services-modal-close')
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((btn, i) => {
+    btn.addEventListener('click', () => {
+        modal(i)
+    })
+})
+
+modalClose.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        modalViews.forEach((modal) => {
+            modal.classList.remove('active-modal')
+        })
+    })
+})
+
+/*==================== PORTFOLIO SWIPER  ====================*/
+let swiperPortfolio = new Swiper(".portfolio-container", {
+    cssMode: true,
+    loop:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+/*==================== TESTIMONIAL ====================*/
+let swiperTestimonial = new Swiper(".testimonial-container", {
+    loop:true,
+    grabCursor: true,
+    spaceBetween: 48,
+
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+
+    breakpoints:{
+        568:{
+            slidesPerView: 2
+        }
+    }
+  });
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight  = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+function scrollHeader(){
+    const nav = document.getElementById('header')
+
+    this.scrollY >= 80? nav.classList.add('scroll-header') : nav.classList.remove('scroll-header');
+}
+
+window.addEventListener('scroll', scrollHeader)
+
+/*==================== SHOW SCROLL UP ====================*/ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up')
+
+    this.scrollY >= 560? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll');
+}
+
+window.addEventListener('scroll', scrollUp)
+
+/*==================== DARK LIGHT THEME ====================*/ 
+const themeButton = document.getElementById('theme-button'),
+      darkTheme = 'dark-theme',
+      iconTheme = 'uil-sun';
+
+const selectedTheme = localStorage.getItem('selected-theme'),
+      selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark-theme' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun';
+
+if(selectedTheme){
+    document.body.classList[selectedTheme === 'dark-theme' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'uil-sun' ? 'add' : 'remove'](iconTheme)
+}
+
+
+themeButton.addEventListener('click', () => {
+
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
