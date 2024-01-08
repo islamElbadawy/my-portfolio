3bf4d913d75cfaa1e2c378cdc1c4f378101cfb42
islamElbadawy <islam.201211@gmail.com>
2024-01-08 18:56:21 +0200

    extract skills data

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -3,10 +3,40 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
   templateUrl: './skills.component.html',
-  styleUrl: './skills.component.css'
+  styleUrls: ['./skills.component.css'],
 })
 export class SkillsComponent {
-
+  skillsData = [
+    {
+      title: 'Frontend developer',
+      subtitle: 'More than 3 years',
+      icon: 'uil uil-brackets-curly',
+      items: [
+        { name: 'HTML', percentage: 90, class: 'skills-html' },
+        { name: 'CSS', percentage: 80, class: 'skills-css' },
+        { name: 'Javascript', percentage: 75, class: 'skills-js' },
+        { name: 'Angular', percentage: 70, class: 'skills-angular' },
+      ],
+    },
+    {
+      title: 'Embedded Systems Engineer',
+      subtitle: 'More than a year',
+      icon: 'uil uil-circuit',
+      items: [
+        { name: 'C/C++', percentage: 50, class: 'skills-c' },
+        { name: 'Microcontroller', percentage: 70, class: 'skills-mic' },
+      ],
+    },
+    {
+      title: 'UI/UX Designer',
+      subtitle: 'More than 3 years',
+      icon: 'uil uil-swatchbook',
+      items: [
+        { name: 'Adobe XD', percentage: 60, class: 'skills-xd' },
+        { name: 'Figma', percentage: 40, class: 'skills-figma' },
+        { name: 'Photoshop', percentage: 75, class: 'skills-ps' },
+      ],
+    },
+  ];
 }
